576de9e962c8faf11f2ff6ce905f700a35161952
darwindev2002 <darwin0630@gmail.com>
2024-02-26 16:49:55 -0800

    Test suggestion from discussion - tbc

--- test/raft_test.go
+++ test/raft_test.go
@@ -2,8 +2,9 @@ package SurfTest
 
 import (
 	"cse224/proj5/pkg/surfstore"
-	emptypb "google.golang.org/protobuf/types/known/emptypb"
 	"testing"
+
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 func TestRaftSetLeader(t *testing.T) {
@@ -72,4 +73,51 @@ func TestRaftSetLeader(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestRaftFollwersGetUpdates(t *testing.T) {
+	t.Log("leader1 gets a request")
+
+	// Setup
+	cfgPath := "./config_files/3nodes.txt"
+	test := InitTest(cfgPath)
+	defer EndTest(test)
+
+	// Set node 0 to be the leader
+	leaderIdx := 0
+	test.Clients[leaderIdx].SetLeader(test.Context, &emptypb.Empty{})
+	test.Clients[leaderIdx].SendHeartbeat(test.Context, &emptypb.Empty{})
+
+	// Here node 0 should be the leader, all other nodes should be followers
+	// All logs and metastores should be empty
+	// TODO: check
+
+	// Update a file on node 0
+	filemeta := &surfstore.FileMetaData{
+		Filename:      "testfile",
+		Version:       1,
+		BlockHashList: nil,
+	}
+	test.Clients[leaderIdx].UpdateFile(test.Context, filemeta)
+	test.Clients[leaderIdx].SendHeartbeat(test.Context, &emptypb.Empty{})
+
+	// One final call to sendhearbeat (from spec)
+	test.Clients[leaderIdx].SendHeartbeat(test.Context, &emptypb.Empty{})
+
+	// Check that all the nodes are in the right state
+	// Leader and all followers should have the entry in the log
+	// Everything should be term 1
+	// Entry should be applied to all metastores
+	// Only node 0 should be the elader
+	goldenMeta := make(map[string]*surfstore.FileMetaData)
+	goldenMeta[filemeta.Filename] = filemeta
+
+	goldenLog := make([]*surfstore.UpdateOperation, 0)
+	goldenLog = append(goldenLog, &surfstore.UpdateOperation{
+		Term:         1,
+		FileMetaData: filemeta,
+	})
+
+	// term := int64(1)
+	// var leader bool
 }
